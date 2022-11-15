661ae50974d5602be0ca725defd3bbde6fe4d29e
Gabi <gabiazcona@protonmail.com>
2022-11-15 11:57:15 +0100

    add ball collision with paddle and walls

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use tetra::{Context, ContextBuilder, State};
-use tetra::graphics::{self, Color, Texture};
+use tetra::graphics::{self, Color, Texture, Rectangle};
 use tetra::input::{self, Key};
 use tetra::math::Vec2;
 
@@ -34,6 +34,23 @@ impl Entity {
             velocity,
         }
     }
+
+    fn width(&self) -> f32 {
+        self.texture.width() as f32
+    }
+
+    fn height(&self) -> f32 {
+        self.texture.height() as f32
+    }
+
+    fn bounds(&self) -> Rectangle {
+        Rectangle::new(
+            self.position.x,
+            self.position.y,
+            self.width(),
+            self.height(),
+        )
+    }
 }
 
 struct GameState {
@@ -63,6 +80,27 @@ impl State for GameState {
         }
         self.ball.position += self.ball.velocity;
 
+        let paddle_bounds = self.paddle.bounds();
+        let ball_bounds = self.ball.bounds();
+
+        let paddle_hit = if ball_bounds.intersects(&paddle_bounds) {
+            Some(&self.paddle)
+        } else { 
+            None 
+        };
+
+        if paddle_hit.is_some() {
+            self.ball.velocity.y = -self.ball.velocity.y;
+        }
+
+        if self.ball.position.x <= 0.0 
+        || self.ball.position.x + self.ball.width() >= WINDOW_WIDTH {
+            self.ball.velocity.x = -self.ball.velocity.x;
+        }
+        if self.ball.position.y  <= 0.0 {
+            self.ball.velocity.y = -self.ball.velocity.y;
+        }
+
         Ok(())
     }
 }
@@ -71,7 +109,7 @@ impl GameState {
     fn new(ctx: &mut Context) -> tetra::Result<GameState> {
         let paddle_texture = Texture::new(ctx, "./resources/paddleBlu.png")?;
         let paddle_position = Vec2::new((WINDOW_WIDTH - paddle_texture.width() as f32) / 2.0, WINDOW_HEIGHT - paddle_texture.height() as f32);
-        
+         
         let ball_texture = Texture::new(ctx, "./resources/ballGrey.png")?;
         let ball_position = Vec2::new(
             WINDOW_WIDTH / 2.0 - ball_texture.width() as f32 / 2.0,

ae83d8b5abaea8942860864a6ce7e4d4a85d869c
Gabi <gabiazcona@protonmail.com>
2022-11-15 12:53:18 +0100

    add you lost text

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use tetra::{Context, ContextBuilder, State, window};
+use tetra::graphics::text::{Text, Font};
+use tetra::{Context, ContextBuilder, State};
 use tetra::graphics::{self, Color, Texture, Rectangle};
 use tetra::input::{self, Key};
 use tetra::math::Vec2;
@@ -66,55 +67,68 @@ impl Entity {
 struct GameState {
     paddle: Entity,
     ball: Entity,
+    lost_text: Text,
+    game_end: bool,
 }
 
 impl State for GameState {
     fn draw(&mut self, ctx: &mut Context) -> tetra::Result {
         graphics::clear(ctx, Color::rgb(0.992,0.0, 0.329));
-
+        
         self.paddle.texture.draw(ctx, self.paddle.position);
         self.ball.texture.draw(ctx, self.ball.position);
 
+        if self.game_end {
+            self.lost_text.draw(ctx, Vec2::new(WINDOW_WIDTH/2.0, WINDOW_HEIGHT/2.0));
+        }
         Ok(())
     }
 
     fn update(&mut self, ctx: &mut Context) -> tetra::Result {
-        if input::is_key_down(ctx, Key::Left) 
-            && (self.paddle.position.x > 0.0) {
-                self.paddle.position.x -= PADDLE_SPEED;
+        if !self.game_end {
+            if input::is_key_down(ctx, Key::Left) 
+                && (self.paddle.position.x > 0.0) {
+                    self.paddle.position.x -= PADDLE_SPEED;
+            }
+            if input::is_key_down(ctx, Key::Right) 
+                // not move past window border
+                && (self.paddle.position.x + (self.paddle.texture.width() as f32) < WINDOW_WIDTH) {
+                    self.paddle.position.x += PADDLE_SPEED;
+            }
+            self.ball.position += self.ball.velocity;
+
+            let paddle_bounds = self.paddle.bounds();
+            let ball_bounds = self.ball.bounds();
+
+            let paddle_hit = if ball_bounds.intersects(&paddle_bounds) {
+                Some(&self.paddle)
+            } else { 
+                None 
+            };
+
+            if let Some(paddle) = paddle_hit {
+                self.ball.velocity.y = -(self.ball.velocity.y + (BALL_ACCELERATION * self.ball.velocity.y.signum()));
+
+                let offset = (paddle.centre().x - self.ball.centre().x) / paddle.width();
+
+                self.ball.velocity.x += PADDLE_SPIN * -offset;
+            }
+
+            if self.ball.position.x <= 0.0 
+            || self.ball.position.x + self.ball.width() >= WINDOW_WIDTH {
+                self.ball.velocity.x = -self.ball.velocity.x;
+            }
+            if self.ball.position.y  <= 0.0 {
+                self.ball.velocity.y = -self.ball.velocity.y;
+            }
+
+            // check if player loses
+            if self.ball.position.y >= WINDOW_HEIGHT {
+                self.game_end = true;
+                println!("YOU LOST!");
+            }
+
         }
-        if input::is_key_down(ctx, Key::Right) 
-            // not move past window border
-            && (self.paddle.position.x + (self.paddle.texture.width() as f32) < WINDOW_WIDTH) {
-                self.paddle.position.x += PADDLE_SPEED;
-        }
-        self.ball.position += self.ball.velocity;
-
-        let paddle_bounds = self.paddle.bounds();
-        let ball_bounds = self.ball.bounds();
-
-        let paddle_hit = if ball_bounds.intersects(&paddle_bounds) {
-            Some(&self.paddle)
-        } else { 
-            None 
-        };
-
-        if let Some(paddle) = paddle_hit {
-            self.ball.velocity.y = -(self.ball.velocity.y + (BALL_ACCELERATION * self.ball.velocity.y.signum()));
-
-            let offset = (paddle.centre().x - self.ball.centre().x) / paddle.width();
-
-            self.ball.velocity.x += PADDLE_SPIN * -offset;
-        }
-
-        if self.ball.position.x <= 0.0 
-        || self.ball.position.x + self.ball.width() >= WINDOW_WIDTH {
-            self.ball.velocity.x = -self.ball.velocity.x;
-        }
-        if self.ball.position.y  <= 0.0 {
-            self.ball.velocity.y = -self.ball.velocity.y;
-        }
-
         Ok(())
     }
 }
@@ -131,7 +145,13 @@ impl GameState {
         );
         let ball_velocity = Vec2::new(0.0, BALL_SPEED);
         
+        let lost_text = Text::new(
+            "You lost!",
+            Font::vector(ctx, "./resources/DejaVuSansMono.ttf", 36.0)?,
+        );
         Ok(GameState {
+            game_end: false,
+            lost_text: lost_text,
             paddle: Entity::new(paddle_texture, paddle_position),
             ball: Entity::with_velocity(ball_texture, ball_position, ball_velocity),
         })
